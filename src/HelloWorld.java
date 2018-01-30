334f1a54b5c206b6f9084763e827b7504fee73af
kidon <kidon@192.168.219.103>
2018-01-30 19:13:21 +0900

    asdasd 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String[] orgs) {
 		System.out.println("Hello Javajigijigi");
 		System.out.println("sex");
+		System.out.println("asdasd");
 	}
 	
 } 
